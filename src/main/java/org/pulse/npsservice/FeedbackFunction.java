857ac11120a09e080eb956ca16bcf061490cddb9
jpcapobiango <joao.capobiango213@gmail.com>
2025-11-11 00:15:31 -0300

    quarkus first commit

---
+++ src/main/java/org/pulse/npsservice/FeedbackFunction.java
@@ -0,0 +1,27 @@
+package org.pulse.npsservice;
+
+import com.microsoft.azure.functions.ExecutionContext;
+import com.microsoft.azure.functions.HttpMethod;
+import com.microsoft.azure.functions.HttpRequestMessage;
+import com.microsoft.azure.functions.HttpResponseMessage;
+import com.microsoft.azure.functions.HttpStatus;
+import com.microsoft.azure.functions.annotation.AuthorizationLevel;
+import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.HttpTrigger;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import java.util.Optional;
+
+@ApplicationScoped
+public class FeedbackFunction {
+    @FunctionName("feedback")
+    public HttpResponseMessage run(
+            @HttpTrigger(
+                    name = "req",
+                    methods = {HttpMethod.GET},
+                    authLevel = AuthorizationLevel.ANONYMOUS)
+            HttpRequestMessage<Optional<String>> request,
+            final ExecutionContext context) {
+        return request.createResponseBuilder(HttpStatus.OK).body("Sending feedback...").build();
+    }
+}
