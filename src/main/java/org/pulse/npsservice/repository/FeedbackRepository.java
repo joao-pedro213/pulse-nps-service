3636100f06ded226b292c1a0b6f89198213d13d5
jpcapobiango <joao.capobiango213@gmail.com>
2026-01-07 21:36:04 -0300

    fix repository query for weekly-report

--- src/main/java/org/pulse/npsservice/repository/FeedbackRepository.java
+++ src/main/java/org/pulse/npsservice/repository/FeedbackRepository.java
@@ -3,14 +3,16 @@ package org.pulse.npsservice.repository;
 import io.quarkus.mongodb.panache.reactive.ReactivePanacheMongoRepository;
 import io.smallrye.mutiny.Uni;
 import jakarta.enterprise.context.ApplicationScoped;
+import org.bson.Document;
 import org.pulse.npsservice.model.FeedbackModel;
 
 import java.time.Instant;
+import java.util.Date;
 import java.util.List;
 
 @ApplicationScoped
 public class FeedbackRepository implements ReactivePanacheMongoRepository<FeedbackModel> {
     public Uni<List<FeedbackModel>> findByDateRange(Instant startDate, Instant endDate) {
-        return find("createdAt >= ?1 and createdAt < ?2", startDate, endDate).list();
+        return find(new Document("createdAt", new Document("$gte", Date.from(startDate)).append("$lt", Date.from(endDate)))).list();
     }
 }
